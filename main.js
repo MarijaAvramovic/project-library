baf4319f02a8116dfab900eb771268699ca7a35f
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2025-11-01 15:45:19 -0400

    jf

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log("e");
+ 
+let myLibrary = [];  
+
+function Book(author, title, numberOfPages, isRead) {
+     if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+                 }
+  this.id = crypto.randomUUID();
+  this.author = author;
+  this.title = title;
+  this.numberOfPages = numberOfPages;
+  this.isRead = isRead;
+   
+}
+
+function addBookToLibrary(author, title, numberOfPages, isRead) {
+     let book = new Book(author, title, numberOfPages, isRead);
+     myLibrary.push(book);
+}
+
+ addBookToLibrary("didika", "didika", 54, true);
+  addBookToLibrary("mimika", "didika", 54, true);
+   addBookToLibrary("pipika", "didika", 483, false);
+console.log(myLibrary);
+
+myLibrary.forEach(booky => {
+  console.log(booky);
+});
+
+console.log(myLibrary[1]["title"]);
