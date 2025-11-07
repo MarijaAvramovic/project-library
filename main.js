2acb1bdea3a019f81415c58ed73cf21254b28710
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2025-11-07 01:08:35 -0500

    add

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const btn = document.querySelector('.btn');
 const btnSaveBook = document.querySelector('#btnSaveBook');
 
 
+
 function Book(author, title, numberOfPages, isRead) {
      if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
@@ -16,62 +17,20 @@ function Book(author, title, numberOfPages, isRead) {
   this.numberOfPages = numberOfPages;
   this.isRead = isRead;
    
-}
+};
 
-function addBookToLibrary(author, title, numberOfPages, isRead) {
-     let book = new Book(author, title, numberOfPages, isRead);
-     myLibrary.push(book);
-}
+ Book.prototype.toggleStatus = function() {
 
- addBookToLibrary("didika", "didika", 54, true);
-  addBookToLibrary("mimika", "didika", 54, true);
-   addBookToLibrary("pipika", "didika", 483, false);
-    addBookToLibrary("mimika", "didika", 565, true);
-   addBookToLibrary("pipika", "didika", 43, false);
- 
+  let isReadBook = this.isRead;
+       if (isReadBook == true){
+        this.isRead = false;
+        
+       }
+       else {
+        this.isRead = true;
+       }
+      };
 
-
-function displayBook(author, title, numOfPages, isRead, id) {
-
-       
-  let newDiv = document.createElement('div');
-      newDiv.classList.add('card');
-      let authorParagraph = document.createElement('p');
-      authorParagraph.textContent =  `Author: ${author}` ;
-      let titleParagraph = document.createElement('p');
-      titleParagraph.textContent =  `Title: ${title}` ;
-      let pagesParagraph = document.createElement('p');
-      pagesParagraph.textContent =  `Number of pages: ${numOfPages}` ;
-      let isReadParagraph = document.createElement('p');
-      isReadParagraph.textContent =  `READ: ${isRead}` ;
-      newDiv.appendChild(titleParagraph);
-      newDiv.appendChild(authorParagraph);
-      newDiv.appendChild(pagesParagraph);
-      newDiv.appendChild(isReadParagraph);
-
-     
-
-      let btnRemoveBook = document.createElement('button');
-      btnRemoveBook.textContent = "Remove book";  
-    btnRemoveBook.classList.add("btnRemoveBook"); 
-    btnRemoveBook.dataset.btnId = id;
-    newDiv.dataset.divId = id;
-      newDiv.appendChild(btnRemoveBook);
-
-     
-
-      container.appendChild(newDiv); 
-       
-} 
-
- 
- 
-    myLibrary.forEach(booky => {
-      displayBook(booky.author, booky.title, booky.numberOfPages, booky.isRead, booky.id);
-
-    });
-
- 
 
 
 btn.addEventListener('click', function() {
@@ -94,6 +53,71 @@ form.addEventListener('submit', function(event) {
 
 
 });
+
+function addBookToLibrary(author, title, numberOfPages, isRead) {
+     let book = new Book(author, title, numberOfPages, isRead);
+     myLibrary.push(book);
+};
+
+ addBookToLibrary("didika", "didika", 54, true);
+  addBookToLibrary("mimika", "didika", 54, true);
+   addBookToLibrary("pipika", "didika", 483, false);
+    addBookToLibrary("mimika", "didika", 565, true);
+   addBookToLibrary("pipika", "didika", 43, false);
+ 
+
+
+function displayBook(author, title, numOfPages, isRead, id) {
+
+       
+  let newDiv = document.createElement('div');
+      newDiv.classList.add('card');
+      let authorParagraph = document.createElement('p');
+      authorParagraph.textContent =  `Author: ${author}` ;
+      let titleParagraph = document.createElement('p');
+      titleParagraph.textContent =  `Title: ${title}` ;
+      let pagesParagraph = document.createElement('p');
+      pagesParagraph.textContent =  `Number of pages: ${numOfPages}` ;
+      let isReadParagraph = document.createElement('p');
+      isReadParagraph.textContent =  `READ: ${isRead}` ;
+      let isReadBtn = document.createElement('button');
+      isReadBtn.textContent =  "X" ;
+       isReadBtn.classList.add("toggleBtn"); 
+       let myToggleBtn = document.createElement('button');
+       myToggleBtn.classList.add("green"); 
+
+      newDiv.appendChild(titleParagraph);
+      newDiv.appendChild(authorParagraph);
+      newDiv.appendChild(pagesParagraph);
+      isReadParagraph.appendChild(isReadBtn);
+      newDiv.appendChild(myToggleBtn);
+      newDiv.appendChild(isReadParagraph);
+
+     
+
+      let btnRemoveBook = document.createElement('button');
+      btnRemoveBook.textContent = "Remove book";  
+    btnRemoveBook.classList.add("btnRemoveBook"); 
+    btnRemoveBook.dataset.btnId = id;
+    newDiv.dataset.divId = id;
+    isReadBtn.dataset.read = id;
+    myToggleBtn.dataset.green = id;
+      newDiv.appendChild(btnRemoveBook);
+
+     
+
+      container.appendChild(newDiv); 
+       
+} ;
+
+ 
+ 
+    myLibrary.forEach(booky => {
+      displayBook(booky.author, booky.title, booky.numberOfPages, booky.isRead, booky.id);
+
+    });
+
+ 
 const buttonsRemoveBook = document.querySelectorAll('.btnRemoveBook');
 
 buttonsRemoveBook.forEach(button => {
@@ -119,3 +143,31 @@ buttonsRemoveBook.forEach(button => {
      
   });
 });
+
+const buttonsToggleStatus = document.querySelectorAll('.toggleBtn');
+
+buttonsToggleStatus.forEach(button => {
+  button.addEventListener('click', (e) => {
+     
+    const clickedBtn = e.currentTarget;
+
+    const dataset = clickedBtn.dataset; 
+    const clickedBtnId = dataset.read;
+
+ 
+ const toggledBook = myLibrary.find(book => book.id === clickedBtnId);
+ console.log(toggledBook);
+    toggledBook.toggleStatus();
+     console.log(toggledBook);
+
+     const greenBtn = document.querySelector(`[data-green="${clickedBtnId}"]`);
+     console.log(greenBtn);
+     if(toggledBook.isRead == false) {
+      greenBtn.classList.toggle('green', false);
+     }else {
+       greenBtn.classList.toggle('green', true);
+     }
+     
+  });
+});
+    
